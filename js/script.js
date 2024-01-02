39a7c9f08b1cd3f457ea99e644295d8fec754cd7
kKaii61 <21h1120041@ut.edu.vn>
2024-01-02 16:22:49 +0700

    update v2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-var link = document.querySelectorAll('a.ta');
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content');
+const leftnav = document.getElementById('left-nav');
+const prod = document.getElementById('content-product');
 
-link.forEach(li => {
-    li.classList.add("active");
-    li.innerHTML = "hi";
-});
-
+tabs.forEach( tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active');
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        if(target == prod) {
+            leftnav.classList.add('hide');
+        } else {
+            leftnav.classList.remove('hide');
+        }
+        tab.classList.add('active');
+        target.classList.add('active');
+    })
+})
